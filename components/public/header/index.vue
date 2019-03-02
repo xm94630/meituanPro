c61466ebd789c9cf60046507afaeca5b167b0f41
xm94630 <xm94630@126.com>
2019-03-02 10:19:42 +0800

    mod

---
+++ components/public/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6">
+      <geo />
+    </el-col>
+    <el-col :span="6">
+      222
+    </el-col>
+    <el-col :span="6">
+      3333
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo.vue'
+export default {
+  components:{
+    Geo
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss";
+</style>
